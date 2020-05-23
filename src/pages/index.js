56ebc64e9c8aed70cb64a3be4520b1534a981019
Ryo Kuroyanagi <ku6ryo@gmail.com>
2020-05-24 03:55:14 +0900

    added animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,53 +5,80 @@ import Logo from "../images/logo.svg"
 import Hexagon from "../images/hexagon.svg"
 import styles from "./index.module.scss"
 
-console.log(styles)
+export default class IndexPage extends React.Component {
 
-const hexaPositions = Array(10).fill(null).map(() => {
-  return [Math.random() * 100, Math.random() * 100, Math.random() * 200, Math.random() * 360]
-})
+  initialState = {
+    hexagons: [],
+  }
 
-const IndexPage = () => (
-  <div className={styles.frame}>
-    <SEO title="Home" />
-    <div className={styles.hexagonShadowLayer}>
-      {hexaPositions.map(pos => {
-        return (
-          <Hexagon key={pos[0]} className={styles.hexagon} style={{
-              left: `calc(${pos[0]}% + 6px)`,
-              top: `calc(${pos[1]}% + 6px)`,
-              width: pos[2] + "px",
-              transform: `rotate(${pos[3]}deg)`
-          }}/>
-        )
-      })}
-    </div>
-    <div className={styles.dotLayer}/>
-    <div className={styles.hexagonLayer}>
-      {hexaPositions.map(pos => {
-        return (
-          <Hexagon key={pos[0]} className={styles.hexagon} style={{
-              left: pos[0] + "%",
-              top: pos[1] + "%",
-              width: pos[2] + "px",
-              transform: `rotate(${pos[3]}deg)`
-          }}/>
-        )
-      })}
-    </div>
-    <div className={styles.contentLayer}>
-      <div className={styles.logoContainer}>
-        <Logo className={styles.logo}/>
+  timeToFloat = 11
+  intervalTime = 500
+
+  constructor(props) {
+    super(props)
+    this.state = this.initialState;
+  }
+
+  componentDidMount() {
+    let id = 0;
+    setInterval(() => {
+      const hexa = {
+        id: id,
+        left: Math.random() * 100,
+        width: 50 + Math.random() * 100,
+        rotation: 30 * Math.random(),
+      }
+      if (id > this.timeToFloat / this.intervalTime * 1000) {
+        this.state.hexagons.pop()
+      }
+      this.setState({
+        hexagons: [hexa, ...this.state.hexagons]
+      })
+      id += 1
+    }, this.intervalTime)
+  }
+
+  render() {
+    const { hexagons } = this.state
+    return (
+      <div className={styles.frame}>
+        <SEO title="Home" />
+        <div className={styles.hexagonShadowLayer}>
+          {hexagons.map(h => {
+            return (
+              <Hexagon key={h.id} className={styles.hexagon} style={{
+                  left: `calc(${h.left}% + 6px)`,
+                  width: `${h.width}px`,
+                  transform: `rotate(${h.rotation}deg)`
+              }}/>
+            )
+          })}
+        </div>
+        <div className={styles.hexagonLayer}>
+          {hexagons.map(h => {
+            return (
+              <Hexagon key={h.id} className={styles.hexagon} style={{
+                  left: `${h.left}%`,
+                  width: `${h.width}px`,
+                  transform: `rotate(${h.rotation}deg)`
+              }}/>
+            )
+          })}
+        </div>
+        <div className={styles.dotLayer}/>
+        <div className={styles.contentLayer}>
+          <div className={styles.logoContainer}>
+            <Logo className={styles.logo}/>
+          </div>
+          <div className={styles.subtitle}>- gallery -</div>
+          <div className={styles.blockContainer}>
+            <div className={styles.block}></div>
+            <div className={styles.block}></div>
+            <div className={styles.block}></div>
+            <div className={styles.block}></div>
+          </div>
+        </div>
       </div>
-      <div className={styles.subtitle}>- gallery -</div>
-      <div className={styles.blockContainer}>
-        <div className={styles.block}></div>
-        <div className={styles.block}></div>
-        <div className={styles.block}></div>
-        <div className={styles.block}></div>
-      </div>
-    </div>
-  </div>
-)
-
-export default IndexPage
+    )
+  }
+}
